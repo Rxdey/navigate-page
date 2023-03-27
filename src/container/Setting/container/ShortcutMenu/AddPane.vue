9017428a1115713cdbae53418122a45b5dbcdf20
jiaoyuhan <jyh1994@qq.com>
2023-03-27 15:08:29 +0800

    🔧 build(build): 打包

--- src/container/Setting/container/ShortcutMenu/AddPane.vue
+++ src/container/Setting/container/ShortcutMenu/AddPane.vue
@@ -17,12 +17,7 @@
           <van-switch v-model="checked" size="24" />
         </template>
       </van-cell>
-      <van-field
-        v-if="checked"
-        label="图标地址"
-        v-model="form.logoUrl"
-        placeholder="请填写图标地址"
-      ></van-field>
+      <van-field v-if="checked" label="图标地址" v-model="form.logoUrl" placeholder="请填写图标地址"></van-field>
     </van-cell-group>
     <van-cell-group :title="checked ? '选择图标' : '图标'">
       <div class="logo-wrap">
@@ -30,26 +25,15 @@
           <div class="image-inner" @click="clearIcon">
             <div class="logo-inner" :style="{ backgroundColor: form.logoBgColor }">
               <div class="logo-img" :style="{ backgroundImage: `url(${currentBg || ''})` }">
-                <span
-                  :style="{ fontSize: `${form.logoLabelSize / 100}rem`, color: form.logoColor }"
-                  v-if="!isSelectIcon"
-                  >{{ form.logoLabel }}</span
-                >
+                <span :style="{ fontSize: `${form.logoLabelSize / 100}rem`, color: form.logoColor }" v-if="!isSelectIcon">{{ form.logoLabel }}</span>
               </div>
             </div>
           </div>
         </div>
         <div class="logo-list">
           <template v-if="!checked">
-            <div
-              class="logo-inner image-inner"
-              :class="{ selected: isSelectIcon }"
-              @click="handleSelectIcon"
-            >
-              <div
-                class="logo-img"
-                :style="{ backgroundImage: tempIco ? `url(${tempIco})` : '' }"
-              ></div>
+            <div class="logo-inner image-inner" :class="{ selected: isSelectIcon }" @click="handleSelectIcon">
+              <div class="logo-img" :style="{ backgroundImage: tempIco ? `url(${tempIco})` : '' }"></div>
             </div>
             <div class="logo-label van-hairline--bottom" v-show="!isSelectIcon">
               <input v-model="form.logoLabel" type="text" placeholder="图标文字(可选)" />
@@ -67,21 +51,11 @@
       <template #title>
         <div class="group-title">
           图标内字体大小
-          <span
-            class="title-right font-color-picker"
-            @click.stop="showFontColorPicker = !showFontColorPicker"
-          ></span>
+          <span class="title-right font-color-picker" @click.stop="showFontColorPicker = !showFontColorPicker"></span>
         </div>
       </template>
 
-      <my-slider
-        v-model="form.logoLabelSize"
-        :step="1"
-        :max="100"
-        :min="30"
-        active-color="#333"
-        button-size="0.4rem"
-      >
+      <my-slider v-model="form.logoLabelSize" :step="1" :max="100" :min="30" active-color="#333" button-size="0.4rem">
         <div class="color-picker" v-show="showFontColorPicker" @click.stop>
           <Chrome v-model="tempFontColor" />
         </div>
@@ -92,20 +66,13 @@
         <div class="color-list">
           <div class="color-item" v-for="(color, i) in colorList" :key="i">
             <div class="color-box image-inner">
-              <span
-                class="color-inner"
-                :style="{ backgroundColor: color }"
-                @click="setColorActive(i)"
-              >
+              <span class="color-inner" :style="{ backgroundColor: color }" @click="setColorActive(i)">
                 <van-icon v-show="colorActive === i" class="color-checked" name="success" />
               </span>
             </div>
           </div>
           <div class="color-item">
-            <div
-              class="color-box image-inner picker"
-              @click.stop="showColorPicker = !showColorPicker"
-            >
+            <div class="color-box image-inner picker" @click.stop="showColorPicker = !showColorPicker">
               <span class="color-inner">
                 <van-icon v-show="tempActive" class="color-checked" name="success" />
               </span>
@@ -119,9 +86,7 @@
     </van-cell-group>
   </div>
   <div class="button-wrap">
-    <van-button v-if="!props.edit" block round type="danger" @click="() => { handlehandleAdded() }"
-      >添加</van-button
-    >
+    <van-button v-if="!props.edit" block round type="danger" @click="() => { handlehandleAdded() }">添加</van-button>
     <div class="button-group" v-else>
       <van-button block round type="default" @click="handleRemove">移除</van-button>
       <van-button block round type="danger" @click="handlehandleAdded(2)">保存</van-button>
@@ -132,46 +97,46 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, computed, watch, Ref } from "vue";
-import { Field as VanField, Toast, Switch as VanSwitch, Dialog } from "vant";
-import { Chrome } from "@ckpack/vue-color";
-import Upload, { UploadExpose } from "@/components/Upload/Upload.vue";
-import { ShortcutData } from "@/common/types";
-import { useStore } from "@/store";
+import { ref, onMounted, computed, watch, Ref } from 'vue';
+import { Field as VanField, Toast, Switch as VanSwitch, Dialog } from 'vant';
+import { Chrome } from '@ckpack/vue-color';
+import Upload, { UploadExpose } from '@/components/Upload/Upload.vue';
+import { ShortcutData } from '@/common/types';
+import { useStore } from '@/store';
 // import { UPDATE_SHORTCUT_LIST } from '@/store/conf';
 
 type TempColor = {
-  rgba?: { r: number; g: number; b: number; a: number };
-};
+  rgba?: { r: number, g: number, b: number, a: number }
+}
 type Props = {
-  title?: string;
-  edit?: boolean;
-  data?: (ShortcutData & { index?: number }) | null;
-};
+  title?: string,
+  edit?: boolean,
+  data?: ShortcutData & { index?: number } | null
+}
 const props = withDefaults(defineProps<Props>(), {
-  title: "自定义导航",
+  title: '自定义导航',
   edit: false,
   data: null,
 });
-const emit = defineEmits(["onSwitch", "close"]);
+const emit = defineEmits(['onSwitch', 'close']);
 const store = useStore();
 const form: Ref<ShortcutData> = ref({
-  sitUrl: "",
-  sitName: "",
-  logoColor: "rgba(255,255,255,1)",
-  logoBg: "",
-  logoBgColor: "",
-  logoLabel: "",
+  sitUrl: '',
+  sitName: '',
+  logoColor: 'rgba(255,255,255,1)',
+  logoBg: '',
+  logoBgColor: '',
+  logoLabel: '',
   logoLabelSize: 37,
-  logoUrl: "",
+  logoUrl: '',
 });
 const colorList = ref([
-  "rgba(0,0,0,0)",
-  "rgba(240,105,91,1)",
-  "rgba(91,242,210,1)",
-  "rgba(246,82,172,1)",
-  "rgba(238,216,78,1)",
-  "rgba(86,214,234,1)",
+  'rgba(0,0,0,0)',
+  'rgba(240,105,91,1)',
+  'rgba(91,242,210,1)',
+  'rgba(246,82,172,1)',
+  'rgba(238,216,78,1)',
+  'rgba(86,214,234,1)',
 ]); // 快速选择色盘
 
 const colorActive = ref(0);
@@ -180,21 +145,21 @@ const checked = ref(false);
 const showFontColorPicker = ref(false);
 const tempActive = ref(false);
 const tempColor: Ref<string | TempColor> = ref({});
-const tempFontColor: Ref<string | TempColor> = ref("rgba(255,255,255,1)");
-const tempIco = ref("");
+const tempFontColor: Ref<string | TempColor> = ref('rgba(255,255,255,1)');
+const tempIco = ref('');
 const isSelectIcon = ref(false);
 const uploadRef = ref<InstanceType<typeof Upload> & UploadExpose>();
 const currentBg = computed(() => (checked.value ? form.value.logoUrl : form.value.logoBg));
 
 onMounted(() => {
-  document.body.addEventListener("click", () => {
+  document.body.addEventListener('click', () => {
     showColorPicker.value = false;
     showFontColorPicker.value = false;
   });
   if (props.edit && props.data) {
     form.value = props.data;
     checked.value = !!props.data.logoUrl;
-    tempColor.value = props?.data?.logoBgColor || "";
+    tempColor.value = props?.data?.logoBgColor || '';
     tempFontColor.value = props.data.logoColor;
   }
 });
@@ -207,7 +172,7 @@ const handleSelectIcon = () => {
 };
 // 清除图标
 const clearIcon = () => {
-  form.value.logoBg = "";
+  form.value.logoBg = '';
   isSelectIcon.value = false;
 };
 // 选择颜色
@@ -233,47 +198,51 @@ const onUpload = (base64: string) => {
 const handlehandleAdded = async (type: 1 | 2 = 1) => {
   const { sitUrl, sitName, logoBg, logoBgColor, logoLabel } = form.value;
   if (!sitUrl) {
-    Toast("请输入网站地址");
+    Toast('请输入网站地址');
     return;
   }
   if (!sitName) {
-    Toast("请输入网站名称");
+    Toast('请输入网站名称');
     return;
   }
-  if (!/[http|https]:\/\//.test(form.value.sitUrl || "")) {
+  // if (!logoBg && !logoLabel) {
+  //   Toast('请完善图标设置');
+  //   return;
+  // }
+  if (!/[http|https]:\/\//.test(form.value.sitUrl || '')) {
     form.value.sitUrl = `http://${form.value.sitUrl}`;
   }
   if (checked.value) {
-    form.value.logoBg = "";
+    form.value.logoBg = '';
   } else {
-    form.value.logoUrl = "";
+    form.value.logoUrl = '';
   }
   const actions = {
-    1: { key: "SAVE_SHORTCUT", name: "添加" },
-    2: { key: "EDIT_SHORTCUT_BY_INDEX", name: "保存" },
+    1: { key: 'SAVE_SHORTCUT', name: '添加' },
+    2: { key: 'EDIT_SHORTCUT_BY_INDEX', name: '保存' },
   };
   //  as keyof typeof actions
   const res = await store.dispatch(actions[type].key, form.value);
   if (!res) {
-    Toast.fail(`${actions[type]["name"]}失败，请重试`);
+    Toast.fail(`${actions[type].name}失败，请重试`);
     return;
   }
-  Toast.success(`${actions[type]["name"]}成功`);
+  Toast.success(`${actions[type].name}成功`);
   if (type === 2) {
-    emit("close");
+    emit('close');
     return;
   }
-  tempIco.value = "";
+  tempIco.value = '';
   clearIcon();
   colorActive.value = 0;
   tempActive.value = false;
   form.value = {
-    sitUrl: "",
-    sitName: "",
-    logoColor: "rgba(255,255,255,1)",
-    logoBg: "",
-    logoBgColor: "",
-    logoLabel: "",
+    sitUrl: '',
+    sitName: '',
+    logoColor: 'rgba(255,255,255,1)',
+    logoBg: '',
+    logoBgColor: '',
+    logoLabel: '',
     logoLabelSize: 37,
   };
 };
@@ -281,17 +250,17 @@ const handlehandleAdded = async (type: 1 | 2 = 1) => {
 const handleRemove = async () => {
   try {
     await Dialog.confirm({
-      title: "注意",
-      message: "确定要删除吗?",
-      theme: "round-button",
+      title: '注意',
+      message: '确定要删除吗?',
+      theme: 'round-button',
     });
-    const res = await store.dispatch("DELETE_SHORTCUT_BY_INDEX", props.data?.index);
+    const res = await store.dispatch('DELETE_SHORTCUT_BY_INDEX', props.data?.index);
     if (!res) {
-      Toast.fail("删除失败, 请重试");
+      Toast.fail('删除失败, 请重试');
       return false;
     }
-    emit("close");
-    Toast("已移除");
+    emit('close');
+    Toast('已移除');
     return res;
   } catch (error) {
     return error;
@@ -300,7 +269,7 @@ const handleRemove = async () => {
 // 取色器格式不对，处理一下
 watch(tempColor, (val) => {
   let color;
-  if (typeof val === "object") {
+  if (typeof val === 'object') {
     const { rgba } = val;
     if (!rgba) return;
     const { r, g, b, a } = rgba;
@@ -316,7 +285,7 @@ watch(tempColor, (val) => {
 });
 watch(tempFontColor, (val) => {
   let color;
-  if (typeof val === "object") {
+  if (typeof val === 'object') {
     const { rgba } = val;
     if (!rgba) return;
     const { r, g, b, a } = rgba;
@@ -329,6 +298,7 @@ watch(tempFontColor, (val) => {
     tempActive.value = true;
   }
 });
+
 </script>
 
 <style lang="less">
@@ -347,7 +317,7 @@ watch(tempFontColor, (val) => {
     width: 70px;
     height: 70px;
     border-radius: 8px;
-    background: rgba(255, 255, 255, 0.5);
+    background: rgba(255,255,255,.5);
     text-align: center;
     line-height: 70px;
     font-size: 40px;
@@ -403,7 +373,11 @@ watch(tempFontColor, (val) => {
       border-style: dashed;
       border: 1px dashed transparent;
       background: linear-gradient(white, white) padding-box,
-        repeating-linear-gradient(-45deg, #ccc 0, #ccc 0.3em, white 0, white 0.52em);
+        repeating-linear-gradient(-45deg,
+          #ccc 0,
+          #ccc 0.3em,
+          white 0,
+          white 0.52em);
     }
   }
 
